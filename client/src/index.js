2a12f6860db64dc7c311f23ee31f8f71ec6d7a5b
Dinesh-Khanal <dinesh_Khanal@hotmail.com>
2022-11-04 21:56:02 +0545

    resolvers and mutation added

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import Page from "./pages";
-import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  HttpLink,
+} from "@apollo/client";
 import { BrowserRouter as Router } from "react-router-dom";
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000",
+  link: new HttpLink({
+    uri: "http://localhost:4000",
+  }),
   cache: new InMemoryCache(),
 });
 
--- index.js
+++ index.js
@@ -1,29 +1,62 @@
 //import express from "express";
 import { gql, ApolloServer } from "apollo-server";
 
+let users = [
+  {
+    id: "7783774847847",
+    name: "Shailesh",
+    email: "shailesh@gmail.com",
+    password: "shailesh123",
+    role: "user",
+  },
+  {
+    id: "779387848476783",
+    name: "Bipin Gyawali",
+    email: "bipin@gmail.com",
+    password: "gyawali123",
+    role: "user",
+  },
+];
+
 const typeDefs = gql`
   type Query {
     getUser: [User]
+    userCount: Int
+    userByName(name: String!): User
   }
   type User {
     id: ID!
-    name: String
-    age: Int
-    office: String
+    name: String!
+    email: String!
+    password: String
+    role: String
+  }
+  type Mutation {
+    addUser(name: String!, email: String!, password: String, role: String): User
   }
 `;
-const mocks = {
-  Query: () => ({
-    getUser: () => [...new Array(6)],
-  }),
-  User: () => ({
-    name: () => "Shailesh",
-    age: () => 30,
-    office: () => "Nimble infosis",
-  }),
+const resolvers = {
+  Query: {
+    getUser: () => users,
+    userCount: () => users.length,
+    userByName: (root, args) => {
+      return users.find((user) => user.name === args.name);
+    },
+  },
+  Mutation: {
+    addUser: (root, args) => {
+      const user = {
+        ...args,
+        id: Math.floor(Math.random() * 10000).toString(),
+      };
+      users = users.concat(user);
+      return user;
+    },
+  },
 };
 
-const server = new ApolloServer({ typeDefs, mocks });
+const server = new ApolloServer({ typeDefs, resolvers });
 server
   .listen()
-  .then(() => console.log("server is up and running on port 4000"));
+  .then(({ url }) => console.log(`server is up and running in ${url}`))
+  .catch((error) => console.log(error.message));
